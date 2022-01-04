39c8131b6aa3a6b44f185f5257829bb145708aee
Vee <vee.jirapong@gmail.com>
2022-01-04 12:32:30 +0700

    Fix:Integrate unique checking

--- cmd/goci/changes_test.go
+++ cmd/goci/changes_test.go
@@ -12,14 +12,14 @@ func TestFilterUnique(t *testing.T) {
 		input  []string
 		output []string
 	}{
-		{"unique values", []string{"example/values.yaml", "example/versions.yaml"}, []string{"example/values.yaml", "example/versions.yaml"}},
-		{"non-unique values", []string{"example/test.yaml", "example/test.yaml"}, []string{"example/test.yaml"}},
+		{"unique values", []string{"example/values.yaml", "example/versions.yaml"}, []string{"example"}},
+		{"non-unique values", []string{"example1/test.yaml", "example2/test.yaml"}, []string{"example1", "example2"}},
 		{"unique and non-unique values", []string{"example1", "example1", "example2"}, []string{"example1", "example2"}},
 	}
 
 	for _, test := range scenarios {
 		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.output, filterUnique(test.input))
+			assert.Equal(t, test.output, filterUnique(test.input, 1))
 		})
 	}
 }
